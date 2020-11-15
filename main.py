21973bf1392041506dd1e026cf700a01ac80eba8
EasyBAMM <huhjb1020@naver.com>
2020-11-15 19:47:49 +0900

    update

--- main.py
+++ main.py
@@ -13,11 +13,33 @@ url_server = {
             }
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 @cross_origin()  
 def home():
-    
+
     return render_template('index.html', url_server=url_server, enumerate=enumerate)
+
+@app.route('/squat.html')
+@cross_origin()  
+def squat():
+
+    return render_template('squat.html', url_server=url_server, enumerate=enumerate)
+
+
+@app.route('/side-legraise.html')
+@cross_origin()  
+def sideLegraise():
+
+    return render_template('side-legraise.html', url_server=url_server, enumerate=enumerate)
+
+
+@app.route('/push-up.html')
+@cross_origin()  
+def pushUp():
+
+    return render_template('push-up.html', url_server=url_server, enumerate=enumerate)
+
+
 
 
 if __name__ == '__main__':
